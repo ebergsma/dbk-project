f80b523b24dae316c7cb442ba8d8ca23afcf6620
Elwin Bergsma <elwin.bergsma@gmail.com>
2016-04-16 19:55:35 +0200

    F5 refresh met opnieuw posten fix toegevoegd.

--- index.php
+++ index.php
@@ -7,6 +7,17 @@
     require_once('system/mysql.php');
     require_once('system/functions.php');
     
+    if (strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') === 0) {
+        $_SESSION['postdata'] = $_POST;
+        header('Location: ' . $_SERVER['PHP_SELF'] . '?'
+        . $_SERVER['QUERY_STRING']);
+        exit;
+    }
+    if (isset($_SESSION['postdata'])) {
+        $_POST = $_SESSION['postdata'];
+        //unset($_SESSION['postdata']);
+    }
+    
     /*
         indien de gebruiker het formulier heeft ingevuld, controleer de input,
         anders niet.
@@ -38,7 +49,7 @@
             isset($_GET['actie'])
             and $_GET['actie'] == 'toon_resultaat'
             and $error_message == ''
-            ){
+    ){        
         // indien gewenst, abboneren we de gebruiker op de nieuwsbrief.
         if (
             array_key_exists('nieuwsbrief', $_POST)
